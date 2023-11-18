dfbf9aa82d3a0895455d0d3baf23b85927c8b95f
Yoshiharu Hirose <yh1224@gmail.com>
2023-11-18 18:45:10 +0900

    added secondary font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,4 @@
-import {Inter} from 'next/font/google';
+import {Inter, Lustria} from 'next/font/google';
 
 export const inter = Inter({subsets: ['latin']});
+export const lusitana = Lustria({subsets: ['latin'], weight: ['400']});
